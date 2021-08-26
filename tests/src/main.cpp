736a29ae5f7d3d83e5d824f5a99bba20243856de
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-26 22:20:44 +0100

    Updates

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/beast-connection-engine/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/Tests.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("NemuBeastConnectionEngine");
+
+    return theTestHarness.run();
+}
