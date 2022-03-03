c0fe5b4a33ede21b42fa8d236a3d88598805f537
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-03 15:44:20 +0000

    Refactoring

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -4,6 +4,8 @@
     See https://github.com/nemu-cpp/beast-connection-engine/blob/main/LICENSE.txt
 */
 
+#include "BeastResponseBuilderTests.h"
+#include "BeastServerTests.h"
 #include <Ishiko/Tests.hpp>
 
 using namespace Ishiko::Tests;
@@ -11,6 +13,10 @@ using namespace Ishiko::Tests;
 int main(int argc, char* argv[])
 {
     TestHarness theTestHarness("NemuBeastConnectionEngine");
+
+    TestSequence& theTests = theTestHarness.tests();
+    theTests.append<BeastResponseBuilderTests>();
+    theTests.append<BeastServerTests>();
 
     return theTestHarness.run();
 }
