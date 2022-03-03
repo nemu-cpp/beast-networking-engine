2953a12bd5883ec21831304e43afcb78e8959506
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-03 17:00:51 +0000

    Refactoring

--- src/BeastListener.cpp
+++ src/BeastListener.cpp
@@ -1,29 +1,12 @@
 /*
-    Copyright (c) 2019-2021 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2019-2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/beast-connection-engine/blob/main/LICENSE.txt
 */
 
 #include "BeastListener.h"
 #include "BeastServer.h"
 #include "BeastSession.h"
-#include "ErrorCategory.h"
 
 namespace Nemu
 {
@@ -36,28 +19,32 @@ BeastListener::BeastListener(BeastServer& server, boost::asio::io_context& ioCon
     m_acceptor.open(endpoint.protocol(), ec);
     if (ec)
     {
-        Fail(error, ec.value(), ec.message(), __FILE__, __LINE__);
+        // TODO: appropriate error
+        Fail(error, ErrorCategory::eGeneric, ec.message(), __FILE__, __LINE__);
         return;
     }
     m_acceptor.set_option(boost::asio::socket_base::reuse_address(true), ec);
     if (ec)
     {
         m_acceptor.close();
-        error.fail(ec.value(), ec.message(), __FILE__, __LINE__);
+        // TODO: appropriate error
+        Fail(error, ErrorCategory::eGeneric, ec.message(), __FILE__, __LINE__);
         return;
     }
     m_acceptor.bind(endpoint, ec);
     if (ec)
     {
         m_acceptor.close();
-        error.fail(ec.value(), ec.message(), __FILE__, __LINE__);
+        // TODO: appropriate error
+        Fail(error, ErrorCategory::eGeneric, ec.message(), __FILE__, __LINE__);
         return;
     }
     m_acceptor.listen(boost::asio::socket_base::max_listen_connections, ec);
     if (ec)
     {
         m_acceptor.close();
-        error.fail(ec.value(), ec.message(), __FILE__, __LINE__);
+        // TODO: appropriate error
+        Fail(error, ErrorCategory::eGeneric, ec.message(), __FILE__, __LINE__);
         return;
     }
 }
